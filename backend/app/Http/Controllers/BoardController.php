6bea575c74b5b56092ca494257f84188fff77e48
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-12 17:40:08 +0900

    ログインしていない状態でメインページに入るとログイン画面に自動で遷移するようにした

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BoardController extends Controller
 {
     public function index() {
-        return view('board.index');
+        if(Auth::check()) {
+            return view('board.index');
+        } else {
+            return redirect('/login');
+        }
     }
 }
