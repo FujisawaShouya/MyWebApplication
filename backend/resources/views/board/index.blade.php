1ff45b9522785d6b4b7040aedd0de326e635a1f0
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-14 10:22:47 +0900

    csrf対策を記述した

--- backend/resources/views/board/index.blade.php
+++ backend/resources/views/board/index.blade.php
@@ -66,6 +66,7 @@
 
         <div class="card-body">
             <form action="/create" method="POST">
+              @csrf
               <div class="row mb-3">
                 <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('※ゲームタイトル') }}</label>
                 <div class="col-md-6">
