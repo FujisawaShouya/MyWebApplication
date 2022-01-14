055e8c5b98a6ee629a1390835cf6b5d5389b93c0
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-14 10:20:08 +0900

    掲載内容をデータベースに保存する処理を記述した

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\BoardRequest;
+use App\Models\Board;
 
 class BoardController extends Controller
 {
@@ -13,5 +15,19 @@ class BoardController extends Controller
         } else {
             return redirect('/login');
         }
+    }
+
+    public function create(BoardRequest $request) {
+        $user = Auth::user();
+        $board = Board::create([
+            'user_id' => $user->id,
+            'title' => $request->title,
+            'platform' => $request->platform,
+            'player' => $request->playerId,
+            'comment' => $request->comment
+        ]);
+        $board->save();
+
+        return redirect('/');
     }
 }
