371a15acda83e6d3bbe90f13dcda710fea1c6648
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-15 21:35:42 +0900

    掲載時間で検索する機能を作成

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\BoardRequest;
 use App\Models\Board;
+use Carbon\Carbon;
 
 class BoardController extends Controller
 {
@@ -32,13 +33,61 @@ class BoardController extends Controller
     }
 
     public function search(Request $request) {
-        
+        $now = Carbon::now();
+        $searched_time = $request->searched_time;
 
-        $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+        if($searched_time == 1) {
+            $added_time = $now->subMinutes(30);
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('created_at', '>=', $added_time)
                         ->where('platform', 'LIKE', "%{$request->searched_platform}%")
                         ->latest()
                         ->get();
 
-        return view('board.search', ['boards' => $boards]);
+            return view('board.search', ['boards' => $boards]);
+        } elseif ($searched_time == 2) {
+            $added_time = $now->subHours(1);
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('created_at', '>=', $added_time)
+                        ->where('platform', 'LIKE', "%{$request->searched_platform}%")
+                        ->latest()
+                        ->get();
+
+            return view('board.search', ['boards' => $boards]);
+        } elseif($searched_time == 3) {
+            $added_time = $now->subDays(1);
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('created_at', '>=', $added_time)
+                        ->where('platform', 'LIKE', "%{$request->searched_platform}%")
+                        ->latest()
+                        ->get();
+
+            return view('board.search', ['boards' => $boards]);
+        } elseif ($searched_time == 4) {
+            $added_time = $now->subDays(2);
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('created_at', '>=', $added_time)
+                        ->where('platform', 'LIKE', "%{$request->searched_platform}%")
+                        ->latest()
+                        ->get();
+
+            return view('board.search', ['boards' => $boards]);
+        } elseif($searched_time == 5) {
+            $added_time = $now->subDays(2);
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('created_at', '<', $added_time)
+                        ->where('platform', 'LIKE', "%{$request->searched_platform}%")
+                        ->latest()
+                        ->get();
+
+            return view('board.search', ['boards' => $boards]);
+        } else {
+            $boards = Board::where('title', 'LIKE',"%{$request->searched_title}%")
+                        ->where('platform', 'LIKE', "%{$request->searched_platform}%")
+                        ->latest()
+                        ->get();
+
+            return view('board.search', ['boards' => $boards]);
+        };
     }
 }
