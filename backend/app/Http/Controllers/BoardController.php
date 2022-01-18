2a035701f203422495c79d1aa86c278798de2090
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-18 17:52:35 +0900

    userモデルを使用できるようにした

--- backend/app/Http/Controllers/BoardController.php
+++ backend/app/Http/Controllers/BoardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\BoardRequest;
 use App\Models\Board;
+use App\Models\User;
 use Carbon\Carbon;
 
 class BoardController extends Controller
