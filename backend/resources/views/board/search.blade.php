eb6cd3cf6d82c9e62896cbd5fb02f63e52c94d28
Shouya FUJISAWA <johnnyshouya@gmail.com>
2022-01-18 17:52:47 +0900

    検索フォームを設置

--- backend/resources/views/board/search.blade.php
+++ backend/resources/views/board/search.blade.php
@@ -1,15 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  @foreach ($boards as $board)
-    <p>{{ $board->title }}</p>
-    <p>{{ $board->platform }}</p>
-  @endforeach
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card" style="width: 54rem;">
+        <div class="card-header">{{ __('フレンド検索') }}</div>
+
+        <div class="card-body">
+          <form action="/search" method="GET">
+            @csrf
+            <div class="row mb-3">
+              <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('※ゲームタイトル') }}</label>
+              <div class="col-md-6">
+                <input id="searched-title" type="text"
+                  class="form-control @error('searched-title') is-invalid @enderror" name="searched_title"
+                  value="{{ old('searched-title') }}" autocomplete="searched-title" placeholder="例&#41; VALORANT">
+                {{-- @error('searched-title')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror --}}
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="title" class="col-md-4 col-form-label text-md-end">{{ __('※機種') }}</label>
+              <div class="col-md-6">
+                <select class="form-select" aria-label="Default select example" name="searched_platform">
+                  <option selected></option>
+                  <option value="PC">PC</option>
+                  <option value="PS4/PS5">PS4/PS5</option>
+                  <option value="Switch">Switch</option>
+                  <option value="スマホ">スマホ</option>
+                  <option value="XboxOne">XboxOne</option>
+                </select>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="time" class="col-md-4 col-form-label text-md-end">{{ __('投稿時間') }}</label>
+              <div class="col-md-6">
+                <select class="form-select" aria-label="Default select example" name="searched_time">
+                  <option selected></option>
+                  <option value="1">30分以内</option>
+                  <option value="2">1時間以内</option>
+                  <option value="3">24時間以内</option>
+                  <option value="4">48時間以内</option>
+                  <option value="5">それ以降</option>
+                </select>
+              </div>
+            </div>
+            <div class="d-md-flex justify-content-md-end">
+              <button type="submit" class="btn btn-outline-primary">検索</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div>
+        @foreach ($boards as $board)
+            
+        @endforeach
+      </div>
+      <div class="card card-body" style="width: 54rem;">
+        <h5 class="card-title">カードのタイトル</h5>
+        <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。</p>
+        <a href="#" class="btn btn-primary">ボタン</a>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
