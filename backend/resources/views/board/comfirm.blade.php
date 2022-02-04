6ea03efef5c379acec8fb6a699d3a9a6800f597a
Sho-ya <82014736+FujisawaShouya@users.noreply.github.com>
2022-02-04 17:06:09 +0900

    確認画面を実装

--- backend/resources/views/board/comfirm.blade.php
+++ backend/resources/views/board/comfirm.blade.php
@@ -4,12 +4,25 @@
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">{{ __('フレンドになりますか') }}</div>
-
-        <div class="card-body">
+        <div class="card card-body">
           @foreach ($comfirms as $comfirm)
-            <p>{{ $comfirm->title }}</p>
+          <table>
+            <tr>
+              <td style="width: 50%">
+                <p>{{ $comfirm->user->name }} さん</p>
+                <h5>DiscordIDはボタンを押すと表示されます</h5>
+                <form action="/mypage" action="GET">
+                  <input type="hidden" name="id" value="{{ $comfirm->id }}">
+                  <button type="submit" class="btn btn-outline-primary">フレンドになる</button>
+                </form>
+              </td>
+              <td>
+                <h5>{{ $comfirm->title }} （{{ $comfirm->platform }}）</h5>
+                <p>{{ $comfirm->player }}</p>
+                <p style="white-space:pre-wrap;">{{ $comfirm->comment }}</p>
+              </td>
+            </tr>
+          </table>
           @endforeach
         </div>
       </div>
